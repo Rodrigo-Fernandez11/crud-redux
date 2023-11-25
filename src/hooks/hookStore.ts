1b0a1af56935b5e55e429e9b0afdda0ff2d59841
Rodrigo-Fernandez11 <101433818+Rodrigo-Fernandez11@users.noreply.github.com>
2023-11-24 22:28:49 -0300

    document code

--- src/hooks/hookStore.ts
+++ src/hooks/hookStore.ts
@@ -1,9 +1,13 @@
-import type { TypedUseSelectorHook } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux"; //importamos los tipos
+import type { AppDispatch, RootState } from "../store"; //importamos los tipos
+ // importamos useDispatch = "forma de enviar acciones" 
+ // useSelector = "optener parte especifica del estado"
 import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "../store";
 
-// Definimos un gancho personalizado para seleccionar parte del estado en componentes de React
+//es una buena practica por que nos resuelve los errores de Typescript
+
+// Definimos un custom hook para seleccionar parte del estado en componentes
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-// Definimos un gancho personalizado para obtener la función 'dispatch' del store en componentes de React
+// Definimos un custom hook para obtener la función 'dispatch' del store en componentes
 export const useAppDispatch: () => AppDispatch = useDispatch;
